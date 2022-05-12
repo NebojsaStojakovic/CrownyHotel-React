4195dc44856d981eede6c9d54a17111f21f05cbc
NebojsaStojakovic <nebojsastojakovicla@gmail.com>
2022-05-12 17:04:25 +0200

    added new componenets

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import {Routes, Route, BrowserRouter} from 'react-router-dom'
 import Navbar from './common/navbar/Navbar';
 import './index.css'
+import Home from './components/pages/Home';
+import About from './components/about/About';
+import Gallery from './components/gallery/Gallery';
+import Destinations from './components/destinations/Destinations';
 
 function App() {
   return (
@@ -8,7 +12,10 @@ function App() {
       <BrowserRouter>
       <Navbar />
         <Routes>
-          <Route />
+          <Route path='/' exact element={<Home />}/>
+          <Route path='/about' exact element={<About />} />
+          <Route path='/gallery' exact element={<Gallery />} />
+          <Route path='/destinations' exact element={<Destinations />} />
         </Routes>
       </BrowserRouter>
     </>
---
+++ src/components/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Hero from "../homeSection/Hero";
+import HomeAbout from "../homeSection/HomeAbout";
+
+const Home = () => {
+  return (
+    <>
+      <Hero />
+      <HomeAbout />
+    </>
+  );
+};
+
+export default Home;
